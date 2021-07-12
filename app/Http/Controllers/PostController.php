d02d37d6a3e3491a3eaad27049dbec68a448e032
akash7051gautam <akashdeep.gautam@ditstek.com>
2021-07-12 19:15:06 +0530

    new changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -98,7 +98,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request,$id)
     {
         if ($request->has('id')) {
             Post::find($request->input('id'))->delete();
